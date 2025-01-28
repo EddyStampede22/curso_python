fbb0509d997888d037cd2fce7029f2ddcb86bad6
EddyStampede22 <pimbert.chabacano@gmail.com>
2025-01-28 14:37:59 -0700

    agregue para interactuar con la linea de comandos jsjsjs

--- linea/app.py
+++ linea/app.py
@@ -1,14 +1,19 @@
 '''
 Archivo Principal de linea
 '''
+import argparse
 import funciones
 def main(m,b):
     X=[x/10.5 for x in range(1,101,10)]
     Y=[funciones.calcular_y(x,m,b) for x in X] 
-    print(X)
-    print(Y)
+    #print(X)
+    #print(Y)
     coordenadas = list(zip(X,Y))
     print(coordenadas)
 
 if __name__=="__main__":
-    main(2,3)
+    parser = argparse.ArgumentParser(description='Calcula las coordenadas de una linea recta')
+    parser.add_argument('-m',type=float,help='Pendiente de la linea',default=2.0)
+    parser.add_argument('-b',type=float,help='Interseccion en y',default=3.0)
+    args=parser.parse_args()
+    main(args.m,args.b)
