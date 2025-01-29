30d0b5b36dfbb0065f588935e2e21036c718af12
EddyStampede22 <pimbert.chabacano@gmail.com>
2025-01-29 14:47:42 -0700

    se agrego un diccionario para poder interactuar con el gato

--- curso_ds4/gato/tablero.py
+++ curso_ds4/gato/tablero.py
@@ -1,6 +1,7 @@
 '''
 Tablero.py: Dibuja el tablero del juego del gato
 '''
+import random 
 def dibuja_tablero(simbolos:dict):
     '''
     Dibuja el tablero del juego del gato
@@ -13,8 +14,17 @@ def dibuja_tablero(simbolos:dict):
           {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
           '''
     )
-    if __name__== '__main__':
-        numeros =[str(i) for i in range(1,10)]
-        dic_simbolos={x:x for x in numeros}
-        dibuja_tablero(dic_simbolos)
-    
+if __name__== '__main__':
+    contador =0
+    numeros =[str(i) for i in range(1,10)]
+    dic_simbolos={x:x for x in numeros}
+    dibuja_tablero(dic_simbolos)
+    x = random.choice(numeros)
+    numeros.remove(x)
+    dic_simbolos[x]='X'
+    dibuja_tablero(dic_simbolos)
+    o= random.choice(numeros) 
+    numeros.remove(o)
+    dic_simbolos[o]='O'
+    dibuja_tablero(dic_simbolos)
+    print(numeros)  
