1ee0775873922f8722b40b439ec8c9065e3e2be7
EddyStampede22 <pimbert.chabacano@gmail.com>
2025-02-02 17:40:58 -0700

    se le agregaron mas funciones al juego del gato

--- curso_ds4/gato/tablero.py
+++ curso_ds4/gato/tablero.py
@@ -22,13 +22,46 @@ def ia(simbolos:dict):
         if simbolos[x] not in ['X','O']:
             simbolos[x]='O'
             ocupado = False
-
+def usuario(simbolos:dict):
+    '''Estrategia del usuario'''
+    ocupado = True
+    lista_numeros =[str(i) for i in range(1,10)]
+    while ocupado is True:
+        x= input('Elija un número del 1 al 9: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X','O']:
+                simbolos[x]= 'X'
+                ocupado=False
+            else:
+                print('Ta ocupado')
+        else:
+            print('del 1 al 9, pesado')
+def juego(simbolos:dict):
+    '''juego del gato'''
+    lista_combinaciones=[
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]
+           ]
+def checa_ganador(simbolos:dict,combinaciones:list):
+    '''Checa si hay un ganador'''
+    for c in combinaciones:
+        if simbolos[c[0]] == simbolos[c[1]] == simbolos[c[2]]:
+            return simbolos[c[0]]
+    return None
 if __name__== '__main__':
     contador =0
     numeros =[str(i) for i in range(1,10)]
     dic_simbolos={x:x for x in numeros}
     dibuja_tablero(dic_simbolos)
     ia(dic_simbolos)
+    dibuja_tablero(dic_simbolos)
+    usuario(dic_simbolos)
     dibuja_tablero(dic_simbolos)
 
 
