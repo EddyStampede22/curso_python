cb14f912bbfbca6cb81ef58d4c10cf25534e9619
EddyStampede22 <pimbert.chabacano@gmail.com>
2025-02-07 20:53:10 -0700

    se actualizaron las funciones de app.py

--- curso_ds4/gato/app.py
+++ curso_ds4/gato/app.py
@@ -6,13 +6,29 @@ un ciclo while.
 import tablero
 def main():
     '''Funcion principal'''
+    X={"g":0,"P":0,"E":0}
+    O={"g":0,"P":0,"E":0}
+    score={"X":X,"O":O}
     numeros=[str(i) for i in range(1,10)]
-    dsimbolos={x:x for x in numeros}
-    g=tablero.juego(dsimbolos)
+    corriendo = True
+    while corriendo:
+        dsimbolos={x:x for x in numeros}
+        g=tablero.juego(dsimbolos)
     if g is not None:
         print(f'El ganador es {g}')
+        if g == 'X':
+            X["G"]+=1
+            O["P"]+=1
+        elif g == 'O':
+            O["G"]+=1
+            X["P"]+=1
+        else:
+            X["E"]+=1
+            O["E"]+=1
     else:
         print('Empate')
+        X["E"]+=1
+        O["E"]+=1
 
 if __name__ == '__main__':
     main()
