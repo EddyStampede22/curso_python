bf5907a19b78f8df0a5e161ffbf93c4bf593f937
EddyStampede22 <pimbert.chabacano@gmail.com>
2025-02-18 14:30:13 -0700

    cambios finales para el ahorcado

--- curso_ds4/ahorcado/app.py
+++ curso_ds4/ahorcado/app.py
@@ -1,8 +1,10 @@
 '''
 Programa principal del juego del ahorcado
 '''
+import os
 import string
 import unicodedata
+import argparse
 from random import choice
 import funciones as fn
 
@@ -27,17 +29,28 @@ def main(archivo_texto:str,nombre_plantilla='plantilla'):
 
     abcdario = {letra:letra for letra in string.ascii_lowercase}
     adivinadas = set()
+    print(p)
 
     while o > 0:
         fn.despliega_plantilla(plantillas,o)
-        fn.adivina_letra(abcdario,p,adivinadas,o)
-        if p==" ".join([letra if letra in adivinadas else '_' for letra in p]):
+        o= fn.adivina_letra(abcdario,p,adivinadas,o)
+        if p=="".join([letra if letra in adivinadas else '_' for letra in p]):
             print("Felicidades! adivinaste la palabra oculta... Maestro!")
             break
-        
+
+    fn.despliega_plantilla(plantillas,o)
+    print(f"la palabra era: {p}, pa la próxima, campeón")    
 if __name__ == '__main__':
-    archivo = './datos/pg15532.txt'
-    main(archivo)
+    parser = argparse.ArgumentParser(description='Juego del Ahorcado')
+    parser.add_argument('-a','--archivo',help='Archivo de texto con palabras a adivinar', default='./datos/pg15532.txt')
+    args = parser.parse_args()
+    archivo = args.archivo
+    if os.stat(archivo) is False:
+        print(f'el archivo {archivo} no exite !')
+
+    #archivo = './datos/pg15532.txt'
+    else:
+        main(archivo)
 
 
 
--- curso_ds4/ahorcado/funciones.py
+++ curso_ds4/ahorcado/funciones.py
@@ -45,7 +45,7 @@ def obten_palabras(lista:list)-> list:
     set_palabras = {unicodedata.normalize('NFKD',palabra).encode('ascii','ignore').decode('ascii') for palabra in set_palabras}
     return list(set_palabras)
 
-def adivina_letra(abc:dict,palabra:str,letras_adivinadas:set,turnos:int):
+def adivina_letra(abc:dict,palabra:str,letras_adivinadas:set,turnos:int) -> int:
     '''
     Adivina una letra de una palabra
 
@@ -71,6 +71,7 @@ def adivina_letra(abc:dict,palabra:str,letras_adivinadas:set,turnos:int):
                 letras_adivinadas.add(letra)
             else:
                 turnos -= 1
+    return turnos
 
 if __name__=='__main__':
     plantillas = carga_plantillas('plantilla')
@@ -92,5 +93,7 @@ if __name__=='__main__':
     abcdario = {letra:letra for letra in string.ascii_lowercase}
     adivinadas = set()
     t = 5 #oportunidades
+    t =adivina_letra(abcdario,p,adivinadas,t)
+    print(t)
     adivina_letra(abcdario,p,adivinadas,t)
-    adivina_letra(abcdario,p,adivinadas,t)
+    print(t)
